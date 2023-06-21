1b948c173c7079b84a2ce88303b57124a872a2e5
NabilNewaz <nabilnewaz@gmail.com>
2023-06-21 11:35:54 +0600

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -48,14 +48,8 @@ function App() {
       axios.post('https://nidv-alidity-checker-backend-bangladesh.vercel.app/check_nid', data)
         .then(function (response) {
           setNIDData(response.data);
-          if (response.data.isValid) {
-            setNIDData(response.data);
-            setLoading(false);
-          }
-          if (!response.data.isValid) {
-            setNIDData(null);
-            setLoading(false);
-          }
+          console.log(NIDData)
+          setLoading(false);
         })
         .catch(function (error) {
           console.log(error);
@@ -68,7 +62,7 @@ function App() {
     <div className="flex items-center justify-center h-screen">
       <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-4">
-          <a href="https://flowbite.com/" className="flex items-center">
+          <a href="https://nidverificationbd.web.app" className="flex items-center">
             <img src="https://i.ibb.co/x7CJ3xt/nidverify.png" className="h-8 mr-2" alt="NIDVerification" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"><span className="font-bold">NID</span>Verification<sup className="text-sm">BD</sup></span>
           </a>
@@ -78,7 +72,7 @@ function App() {
       <div className="mx-auto w-[30rem] content-center px-3 lg:px-0">
         <div className={!NIDData ? "block" : "hidden"}>
           <div className={isLoaded ? "hidden" : "block mb-5"}>
-            <div className="flex items-center animate-pulse justify-center w-full h-60 bg-gray-300 rounded-lg dark:bg-gray-700"> <svg className="w-12 h-12 text-gray-200" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" fill="currentColor" viewBox="0 0 640 512"><path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z"></path></svg> </div>
+            <div className="flex items-center animate-pulse justify-center w-full h-[19rem] bg-gray-300 rounded-xl dark:bg-gray-700"> <svg className="w-12 h-12 text-gray-200" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" fill="currentColor" viewBox="0 0 640 512"><path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z"></path></svg> </div>
           </div>
 
           <div className={isLoaded && !isNID && !isDOB ? "block" : "hidden"}>
@@ -86,11 +80,11 @@ function App() {
           </div>
 
           <div className={isNID ? "block" : "hidden"}>
-            <img onLoad={handleImageLoad} className="h-auto rounded-lg mb-5" src="https://i.ibb.co/Qf1GybZ/nidnum.png" alt="image description" />
+            <img onLoad={handleImageLoad} className="h-auto rounded-lg mb-5" src="https://i.ibb.co/FVXLZ8L/nidnum.png" alt="image description" />
           </div>
 
           <div className={isDOB ? "block" : "hidden"}>
-            <img onLoad={handleImageLoad} className="h-auto rounded-lg mb-5" src="https://i.ibb.co/Q8TFsMp/dob.png" alt="image description" />
+            <img onLoad={handleImageLoad} className="h-auto rounded-lg mb-5" src="https://i.ibb.co/VSs6Zjh/dob.png" alt="image description" />
           </div>
 
           <form onSubmit={handleSubmit}>
@@ -123,10 +117,10 @@ function App() {
         </div>
 
         <div>
-          <div className={NIDData?.isValid && NIDData?.data ? "block" : "hidden"}>
+          <div className={NIDData?.isValid == true && NIDData?.data != null ? "block" : "hidden"}>
             <div id="alert-additional-content-3" className="p-4 mb-4 text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 dark:border-green-800" role="alert">
               <div className="flex items-center">
-                <svg aria-hidden="true" className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+                <svg aria-hidden="true" className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"></path></svg>
                 <span className="sr-only">Info</span>
                 <h3 className="text-lg font-medium">This NID Is Valid</h3>
               </div>
@@ -135,30 +129,68 @@ function App() {
               </div>
               <div className="flex">
                 <button onClick={handelSearchAgain} type="button" className="text-white bg-green-800 hover:bg-green-900 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-xs px-3 py-1.5 mr-2 text-center inline-flex items-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
-                  <svg aria-hidden="true" class="-ml-0.5 mr-2 h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                  <svg aria-hidden="true" className="-ml-0.5 mr-2 h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                   <span className="mb-0.5">Search Again</span>
                 </button>
               </div>
             </div>
             <div>
-              <div class="flex flex-col sm:flex-row bg-white shadow-md rounded-lg overflow-hidden">
-                <div class="pl-4 pt-4 lg:pb-4 sm:w-1/3">
-                <img class="rounded w-36 h-36" src={NIDData?.data?.photo} alt="Extra large avatar"/>
+              <div className="flex flex-col sm:flex-row bg-white shadow-md rounded-lg overflow-hidden">
+                <div className="pl-4 pt-4 lg:pb-4 sm:w-1/3">
+                  <img className="rounded w-36 h-36" src={NIDData?.data?.photo} alt="Extra large avatar" />
                 </div>
-                <div class="p-4 sm:w-2/3">
-                  <h2 class="text-gray-800 font-bold text-2xl mb-2">{NIDData?.data.name}</h2>
-                  <p class="text-gray-600">
-                    পিতার নাম : {NIDData?.data.fathers_name}
+                <div className="p-4 sm:w-2/3">
+                  <h2 className="text-gray-800 font-bold text-2xl mb-2">{NIDData?.data?.name}</h2>
+                  <p className="text-gray-600">
+                    পিতার নাম : {NIDData?.data?.fathers_name}
                   </p>
-                  <p class="text-gray-600">
-                    মাতার নাম : {NIDData?.data.mothers_name}
+                  <p className="text-gray-600">
+                    মাতার নাম : {NIDData?.data?.mothers_name}
                   </p>
-                  <p class="text-gray-600">
-                    জন্ম তারিখ : {NIDData?.data.date_of_birth}
+                  <p className="text-gray-600">
+                    জন্ম তারিখ : {NIDData?.data?.date_of_birth}
                   </p>
                 </div>
               </div>
 
+            </div>
+          </div>
+
+          <div className={NIDData?.isValid == false && NIDData?.data == null ? "block" : "hidden"}>
+            <div id="alert-additional-content-3" className="p-4 mb-4 text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800" role="alert">
+              <div className="flex items-center">
+                <svg aria-hidden="true" className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"></path></svg>
+                <span className="sr-only">Info</span>
+                <h3 className="text-lg font-medium">This NID Is Not Valid</h3>
+              </div>
+              <div className="mt-2 mb-4 text-sm">
+                Please click on Search Again button and check the information again.
+              </div>
+              <div className="flex">
+                <button onClick={handelSearchAgain} type="button" className="text-white bg-red-800 hover:bg-red-900 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-xs px-3 py-1.5 mr-2 text-center inline-flex items-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+                  <svg aria-hidden="true" className="-ml-0.5 mr-2 h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                  <span className="mb-0.5">Search Again</span>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className={NIDData?.isValid == true && NIDData?.data == null ? "block" : "hidden"}>
+            <div id="alert-additional-content-3" className="p-4 mb-4 text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800" role="alert">
+              <div className="flex items-center">
+                <svg aria-hidden="true" className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"></path></svg>
+                <span className="sr-only">Info</span>
+                <h3 className="text-lg font-medium">This NID Is Valid</h3>
+              </div>
+              <div className="mt-2 mb-4 text-sm">
+                Sorry we can not find details information about this NID for match with document but your NID Number is valid.
+              </div>
+              <div className="flex">
+                <button onClick={handelSearchAgain} type="button" className="text-white bg-blue-800 hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-xs px-3 py-1.5 mr-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  <svg aria-hidden="true" className="-ml-0.5 mr-2 h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                  <span className="mb-0.5">Search Again</span>
+                </button>
+              </div>
             </div>
           </div>
         </div>
@@ -166,7 +198,7 @@ function App() {
       </div>
 
       <footer className="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
-        <span className="mx-auto text-sm text-gray-500 text-center dark:text-gray-400">© Copyright 2023. Developed By <a href="https://nabilnewaz.com/" className="hover:underline">Nabil Newaz</a>
+        <span className="mx-auto text-sm text-gray-500 text-center dark:text-gray-400">© Copyright {new Date().getFullYear()}. Developed By <a href="https://nabilnewaz.com/" className="hover:underline">Nabil Newaz</a>
         </span>
       </footer>
 
